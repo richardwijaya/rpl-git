708e705200d3fa36270e070b2698777c699df2f8
RICHARD WIJAYA <i16014@ftis>
2018-11-02 14:30:31 +0700

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator{
+	
+	public static void main(String[] args) {
+        
+    }
+    
+    public float plus(float a, float b){
+        return 0f;
+    }
+    
+    public float minus(float a, float b){
+        return 0f;
+    }
+    
+    public float multiply(float a, float b){
+        return 0f;
+    }
+    
+    public float division(float a, float b){
+        return 0f;
+    }
+	
+}
